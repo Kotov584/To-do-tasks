6722364549b289623d37e20af067f49114d01811
Kotov584 <85634618+Kotov584@users.noreply.github.com>
2022-09-18 08:12:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from os.path import exists
 from os import remove
 
+def get_tasks():
+    tasks = open('tasks.txt', 'r')
+    lines = tasks.readlines()
+    tasks.close()
+    
+    count = 0
+    
+    for line in lines:
+        print("{}: {}".format(count, line.strip()))
+        count += 1
+
 if not exists("tasks.txt"):  
     open('tasks.txt', 'x').close()    
 
 print("Welcome to simple console based To-do task application!\r\nAvailable options:\r\n")
 print("0 - To exit application\r\n1 - Create new task\r\n2 - Show creted tasks\r\n3 - Mark task as done\r\n4 - Remove task\r\n5 - Edit task name")
-
-selectedTask = int(input("\r\nWrite your option: \r\n")) 
  
 while True:
+    selectedTask = int(input("\r\nWrite your option: \r\n")) 
+    
     if selectedTask == 0:
         break
 
@@ -30,15 +41,7 @@ while True:
         if not exists("done_tasks.txt"):  
             open('done_tasks.txt', 'x').close()   
 
-        tasks = open('tasks.txt', 'r')
-        lines = tasks.readlines()
-        tasks.close()
-        
-        count = 0
-        
-        for line in lines:
-            print("{}: {}".format(count, line.strip()))
-            count += 1
+        get_tasks()
 
         selectedTask = int(input("\r\nSelect task to be done by number: \r\n")) 
 
@@ -58,15 +61,7 @@ while True:
         print("You done this task successfully")
 
     if selectedTask == 4:  
-        tasks = open('tasks.txt', 'r')
-        lines = tasks.readlines()
-        tasks.close()
-        
-        count = 0
-        
-        for line in lines:
-            print("{}: {}".format(count, line.strip()))
-            count += 1
+        get_tasks()
 
         selectedTask = int(input("\r\nSelect task to be removed by number: \r\n")) 
 
@@ -82,15 +77,7 @@ while True:
         print("You removed this task successfully")
 
     if selectedTask == 5:
-        tasks = open('tasks.txt', 'r')
-        lines = tasks.readlines()
-        tasks.close()
-        
-        count = 0
-        
-        for line in lines:
-            print("{}: {}".format(count, line.strip()))
-            count += 1
+        get_tasks()
 
         selectedTask = int(input("\r\nSelect task to be edited by number: \r\n")) 
         newTaskName = input("Write new name for this task: \r\n")
